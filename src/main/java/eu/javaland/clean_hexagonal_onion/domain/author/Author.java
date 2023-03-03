67c235c4a160a6f2db40f9c6987fe932b1155ede
Maik Kingma <m.kingma2@dictu.nl>
2023-03-03 13:43:15 +0100

    Added Liquibase migration and migration scripts for author sequence and author table

---
+++ src/main/java/eu/javaland/clean_hexagonal_onion/domain/author/Author.java
@@ -0,0 +1,26 @@
+package eu.javaland.clean_hexagonal_onion.domain.author;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+/**
+ * The Author domain aggregate
+ */
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Author {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Public factory method for an {@link  Author} Aggregate. This method is essentially part of the Domain interaction layer
+     * but resides withing the aggregate itself due to the private access modifier of the all args constructor.
+     *
+     * @param firstName the first name of the author
+     * @param lastName the last name of the author
+     * @return the {@link  Author}
+     */
+    public static Author createAuthor(String firstName, String lastName) {
+        return new Author(null, firstName, lastName);
+    }
+}
