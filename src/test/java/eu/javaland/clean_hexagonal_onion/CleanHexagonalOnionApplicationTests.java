42b6e1dac070f56023832bb012383eaff7fc21ff
Maik Kingma <m.kingma2@dictu.nl>
2023-03-03 10:46:45 +0100

    spring initializr

---
+++ src/test/java/eu/javaland/clean_hexagonal_onion/CleanHexagonalOnionApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.javaland.clean_hexagonal_onion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanHexagonalOnionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
