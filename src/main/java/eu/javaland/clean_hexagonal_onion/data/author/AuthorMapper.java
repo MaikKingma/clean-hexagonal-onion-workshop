e075ed3cacbd97ea5a5d4354b8b6b2e8882fb8dd
Maik Kingma <m.kingma2@dictu.nl>
2023-03-08 14:47:44 +0100

    corrected mapper implementation

--- src/main/java/eu/javaland/clean_hexagonal_onion/data/author/AuthorMapper.java
+++ src/main/java/eu/javaland/clean_hexagonal_onion/data/author/AuthorMapper.java
@@ -4,7 +4,7 @@ import eu.javaland.clean_hexagonal_onion.domain.author.Author;
 
 public class AuthorMapper {
     public static AuthorJPA mapToJPA(Author author){
-        return new AuthorJPA.AuthorJPABuilder()
+        return AuthorJPA.builder()
                 .id(author.getId())
                 .firstName(author.getFirstName())
                 .lastName(author.getLastName())
