37703973c1547ab64a9e7d5c214fdf74118475cc
Maik Kingma <maik.kingma@codecentric.nl>
2023-03-21 23:43:58 +0100

    Merge remote-tracking branch 'origin/4-create-author-command-done' into 5-persist-author-data-done
    # Conflicts:
    #	src/test/java/eu/javaland/clean_hexagonal_onion/command/author/AuthorCommandsTest.java

--- src/test/java/eu/javaland/clean_hexagonal_onion/command/author/AuthorCommandsIntegrationTest.java
+++ src/test/java/eu/javaland/clean_hexagonal_onion/command/author/AuthorCommandsIntegrationTest.java
@@ -1,6 +1,7 @@
 package eu.javaland.clean_hexagonal_onion.command.author;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,15 +22,27 @@ class AuthorCommandsIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @BeforeEach
+    void beforeAll() {
+        authorRepository.deleteAll();
+    }
+
     @Test
-    void register() throws Exception {
+    void registerAndGet() throws Exception {
         //given
         var registerAuthorDTOJson = objectMapper.writeValueAsString(new RegisterAuthorDTO("firstName", "lastName"));
-
-        //when //then
+        var expected = AuthorJPA.builder().firstName("firstName").lastName("lastName").build();
+        //when
         mockMvc.perform(post("/authors/commands/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(registerAuthorDTOJson))
                 .andExpect(status().isAccepted());
+        authorRepository.flush();
+        // then
+        assertThat(authorRepository.findAll().size()).isEqualTo(1);
+        assertThat(authorRepository.findAll().get(0)).usingRecursiveComparison().ignoringFields("id").isEqualTo(expected);
     }
 }
