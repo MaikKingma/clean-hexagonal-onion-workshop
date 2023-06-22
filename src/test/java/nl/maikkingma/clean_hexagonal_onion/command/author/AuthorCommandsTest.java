620b71ee9518139d692bf59b6e6e0573e52efc65
Maik Kingma <m.kingma2@dictu.nl>
2023-06-22 12:35:27 +0200

    renamed package structure

--- src/test/java/nl/maikkingma/clean_hexagonal_onion/command/author/AuthorCommandsTest.java
+++ src/test/java/nl/maikkingma/clean_hexagonal_onion/command/author/AuthorCommandsTest.java
@@ -1,4 +1,4 @@
-package eu.javaland.clean_hexagonal_onion.command.author;
+package nl.maikkingma.clean_hexagonal_onion.command.author;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
