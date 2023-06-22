963223306feaf34e95f72bdec74de176dac1c717
Maik Kingma <m.kingma2@dictu.nl>
2023-06-22 13:54:00 +0200

    updated package naming

--- src/test/java/nl/maikkingma/clean_hexagonal_onion/data/author/AuthorMapperTest.java
+++ src/test/java/nl/maikkingma/clean_hexagonal_onion/data/author/AuthorMapperTest.java
@@ -1,6 +1,6 @@
-package eu.javaland.clean_hexagonal_onion.data.author;
+package nl.maikkingma.clean_hexagonal_onion.data.author;
 
-import eu.javaland.clean_hexagonal_onion.domain.author.Author;
+import nl.maikkingma.clean_hexagonal_onion.domain.author.Author;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- src/test/java/nl/maikkingma/clean_hexagonal_onion/data/author/AuthorServiceImplTest.java
+++ src/test/java/nl/maikkingma/clean_hexagonal_onion/data/author/AuthorServiceImplTest.java
@@ -1,14 +1,14 @@
-package eu.javaland.clean_hexagonal_onion.data.author;
+package nl.maikkingma.clean_hexagonal_onion.data.author;
 
-import eu.javaland.clean_hexagonal_onion.domain.author.Author;
+import nl.maikkingma.clean_hexagonal_onion.domain.author.Author;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.assertArg;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -26,11 +26,11 @@ class AuthorServiceImplTest {
         // when
         authorService.registerAuthor(Author.createAuthor("firstName", "lastName"));
         // then
-        ArgumentCaptor<AuthorJPA> argumentCaptor = ArgumentCaptor.forClass(AuthorJPA.class);
-        verify(authorRepository, times(1)).save(argumentCaptor.capture());
-        assertThat(argumentCaptor.getValue()).usingRecursiveComparison().isEqualTo(AuthorJPA.builder()
-                .firstName("firstName")
-                .lastName("lastName")
-                .build());
+        verify(authorRepository, times(1)).save(assertArg(actual ->
+                assertThat(actual).usingRecursiveComparison().isEqualTo(AuthorJPA.builder()
+                        .firstName("firstName")
+                        .lastName("lastName")
+                        .build())));
+
     }
 }
